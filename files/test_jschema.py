c16c750fe28431d4f62bcc8c6b6a1522bbb428a7
Nicolas Leiva <nicolasleiva@gmail.com>
2021-03-09 15:56:24 -0500

    Update validate state

--- files/test_jschema.py
+++ files/test_jschema.py
@@ -10,3 +10,8 @@ with open('files/neighbors.json') as f:
 for neighbor in data:
     print(json.dumps(neighbor, indent=2))
     validate(instance=neighbor, schema=schema)
+
+# with open('files/schema-array.json') as f:
+#   schema = json.load(f)
+
+# validate(instance=data, schema=schema)
